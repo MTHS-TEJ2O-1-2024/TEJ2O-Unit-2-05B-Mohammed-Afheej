cf1ff34a7da84f680e29587a69591b857e682327
Mohammed-Afheej <mohammed.afheej@mths.ca>
2024-11-10 17:30:22 -0500

    edited the comment

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Mohammed Afheej
+ * Created on: Nov 2024
+  * This program shows the temperature in Kelvin.
 */
 
-basic.showString('Hello, World!')
+// happy face at start
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// display temperature when the a button is pressed
+input.onButtonPressed(Button.A, function () {
+    let findingTemperature = Math.round(input.temperature() + 273.15)
+    basic.clearScreen()
+    basic.showString('The temperature is: ')
+    basic.showNumber(findingTemperature)
+    basic.showString('K.')
+    basic.showIcon(IconNames.Happy)
+})
